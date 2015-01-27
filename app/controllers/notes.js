871830a981e2a7294db8f4bbcad0a4b8c2503362
smturek <smturek@gmail.com>
2015-01-27 00:24:42 -0700

    added delete functionality

--- app/controllers/notes.js
+++ app/controllers/notes.js
@@ -3,15 +3,23 @@ import Ember from "ember";
 export default Ember.ArrayController.extend({
   actions: {
     newNote: function() {
-      var body = this.get('noteCopy');
 
-      var note = this.store.createRecord('note', { body: body });
-      this.set('noteCopy', '');
-      note.save();
+      if('noteCopy' !== "") {
+        var body = this.get('noteCopy');
+
+        var note = this.store.createRecord('note', { body: body });
+        this.set('noteCopy', '');
+
+        note.save();
+      }
+
     },
 
-    deleteNote: function () {
-
+    deleteNote: function (id) {
+      var note = this.store.find('note', id).then(function(note) {
+        note.deleteRecord();
+        note.save();
+      });
     }
   }
 });
